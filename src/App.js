afd8eda54fb573e198f23cf71df59aa7f1a900d3
Chandresh Tiwari <chandreshtj@gmail.com>
2023-03-16 22:43:45 +0530

    feat: added word and sentence counter

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import { useState, useEffect } from "react";
 function App() {
   const [text, setText] = useState("");
   const [characters, setCharacters] = useState(0);
+  const [words, setWords] = useState(0);
+  const [sentences, setSentences] = useState(0);
+  const [paragraph, setParagraph] = useState(0);
+
+  function count(text, pattern) {
+    const matches = text.match(pattern);
+    return matches ? matches.length : 0;
+  }
+
   useEffect(() => {
     setCharacters(text.length);
+    setWords(count(text.trim(), /\s+/g));
+    setSentences(count(text, /[.?!]\s/g));
+    setParagraph(count(text, /\n/g));
   }, [text]);
   return (
     <div className="container">
@@ -18,15 +30,15 @@ function App() {
           Characters: <span id="characterCount">{characters}</span>
         </div>
         <div>
-          Words: <span id="wordCount">0</span>
+          Words: <span id="wordCount">{words}</span>
         </div>
       </div>
       <div className="output row">
         <div>
-          Sentences: <span id="sentenceCount">0</span>
+          Sentences: <span id="sentenceCount">{sentences}</span>
         </div>
         <div>
-          Paragraphs: <span id="paragraphCount">0</span>
+          Paragraphs: <span id="paragraphCount">{paragraph}</span>
         </div>
       </div>
     </div>
